84a7ca69c438014a8119ab6daa10afcd79a86add
zzj <156321781@qq.com>
2022-09-23 14:05:10 +0800

    fix

--- src/main/java/com/study/design/absfactory/factory/AbstractFactory.java
+++ src/main/java/com/study/design/absfactory/factory/AbstractFactory.java
@@ -2,7 +2,6 @@ package com.study.design.absfactory.factory;
 
 import com.study.design.absfactory.interfac.Color;
 import com.study.design.absfactory.interfac.Shape;
-
 public abstract class AbstractFactory {
      public  abstract Color getColor(String color);
 
